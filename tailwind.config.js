908207ec98f82c6e68aedb6051d17c8f47998d94
Khush786Mohammad <khush786moh@gmail.com>
2023-11-28 12:35:04 +0530

    tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*"],
-  theme: {
-    screens: {
-
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-
-      'm2xl': {'max': '1535px'},
-      // => @media (max-width: 1535px) { ... }
-
-      'mxl': {'max': '1279px'},
-      // => @media (max-width: 1279px) { ... }
-
-      'mlg': {'max': '1023px'},
-      // => @media (max-width: 1023px) { ... }
-
-      'mmd': {'max': '767px'},
-      // => @media (max-width: 767px) { ... }
-
-      'msm': {'max': '639px'},
-      // => @media (max-width: 639px) { ... }
-    },
-    
+  theme: {    
     extend: {
       fontFamily: {
         mullish: ["mulish","sans-serif"],
